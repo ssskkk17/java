618787c3fbea6d6dfda199b8999067667567c025
ssskkk17 <not36versatile@gmail.com>
2024-01-18 21:08:56 +0900

    ver.01

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String abc="Hello, everyone.";
+		String aaa="It's a better day than yesterday";
+		System.out.println(abc);
+		System.out.println(aaa);
+	}
+
+}
